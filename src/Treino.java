9a7a63f19913f8df908168d69217d06b7ae7e304
Raphael Barboteu <barboteuraphael@gmail.com>
2023-12-31 16:50:46 -0300

    Adicionando o projeto

--- src/Treino.java
+++ src/Treino.java
@@ -3,28 +3,19 @@ import java.util.Scanner;
 public class Treino {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
-        int perguntaInicial;
-        int medidaQuadrado;
-        double medidaDoCiruclo;
 
-        System.out.println("Digite 1 para calcular a área do quadrado ou 2 para calcular a área do círculo");
-        perguntaInicial = leitura.nextInt();
-        
-        if (perguntaInicial == 1) {
+        double numeroUsuario;
+        int limiteTabuada;
 
-            System.out.println("Digite a medida do lado do quadrado");
-            medidaQuadrado = leitura.nextInt();
-            double areaQuadrado = medidaQuadrado * medidaQuadrado;
-            System.out.println("A área do quadrado é: " + areaQuadrado);
+        System.out.println("Digite o número que você quer saber a tabuada: ");
+        numeroUsuario = leitura.nextDouble();
 
-        } else if (perguntaInicial == 2) {
+        System.out.println("Até qual multiplicador você quer a tabuada?");
+        limiteTabuada = leitura.nextInt();
 
-            System.out.println("Digite o raio do círculo");
-            medidaDoCiruclo = leitura.nextDouble();
-            double areaCirculo = 3.14 * medidaDoCiruclo * medidaDoCiruclo;
-            System.out.println("A área do círculo é: " + areaCirculo);
+        for( int i = 0; i <= limiteTabuada; i++){
+            System.out.println(numeroUsuario + " x " + i + " = " + numeroUsuario * i);
         }
-
-    }
+        }
     }
 
